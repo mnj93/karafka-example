102055a6cfa0c9341289dfb19a2c22dbb0c07883
Manoj Kumawat <manoj.kumawat@coindcx.com>
2019-02-20 13:08:39 +0530

    removed binding.pry

--- app/responders/user_responder.rb
+++ app/responders/user_responder.rb
@@ -2,7 +2,6 @@ class UserResponder < ApplicationResponder
     topic :test
     def respond(user)
         data = {"user" => user}
-        binding.pry
         respond_to :test, data.to_json
     end
 end
